73f09f198c2aab0f236942fc827fbe5df34f1d7a
Philipp Grafendorfer <philipp.grafendorfer@gmail.com>
2018-03-15 22:55:22 +0100

    chapter4 exercises, part1

---
+++ Philipp/stat_rethinking_script.R
@@ -0,0 +1,7 @@
+install.packages(c("coda","mvtnorm","devtools"))
+library(devtools)
+devtools::install_github("rmcelreath/rethinking")
+library(rethinking)
+
+growth <- replicate( 10000 , prod( 1 + runif(12,0,0.1) ) )
+dens( growth , norm.comp=TRUE )
